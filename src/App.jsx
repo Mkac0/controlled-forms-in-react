dfb037fd8f53f3639f12a870b0534c71ee933196
melabreu <melissa.abreu84@gmail.com>
2025-09-18 09:55:15 -0400

    lab progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import Bookshelf from './components/Bookshelf.jsx';
+import { useState } from 'react';
+import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf.jsx';
 
 const App = () => {
   return (
     <>
-    <h1>Hello world!</h1>
-    <Bookshelf />
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
     </>
   );
 };
--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,13 +1,61 @@
 import { useState } from 'react';
 
-const bookshelf = () => {
+const Bookshelf = (props) => {
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+
+    const [newBook, setNewBook] = useState({
+        title: '',
+        author: '',
+    });
+
+    const handleChange = (event) => {
+        setNewBook(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.addBook(newBook);
+        setNewBook('');
+    }
+
     return (
-        <div className="bookshelfDiv">
-        <div className="formDiv">
+        <div className='formDiv'>
             <h3>Add a Book</h3>
-            {/* Form will go here */}
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title </label>
+                <input
+                    id="title"
+                    type='text'
+                    name='title'
+                    onChange={handleChange}
+                    value={newBook.title}
+                />
+
+                <label htmlFor="author">Author </label>
+                <input
+                    id="author"
+                    type='text'
+                    name='author'
+                    onChange={handleChange}
+                    value={newBook.author}
+                />
+
+                <button type="submit">Add Book </button>
+            </form>
+            <div className="bookCardsDiv">
+                {books.map((book, index) => {
+                    <div className="bookCard" key={`${book.title}-${book.author}-${index}`}>
+                        <strong>{book.title}</strong>
+                        <div>{book.author}</div>
+                    </div>
+                })}
+            </div>
         </div>
-        <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-        </div>
-    )
+    );
 }
+
+export default Bookshelf;
